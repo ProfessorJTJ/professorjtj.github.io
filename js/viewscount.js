6ca6f4a2d09e587f6a77162ca4b6c8aa815f0e5e
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2023-06-25 12:55:05 +0330

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -49,7 +49,7 @@
 	}
 	
 	let GetPostID = function() {
-		let viewMoreOften = 488;
+		let viewMoreOften = 493;
 		if ((Math.floor(Math.random() * 2)) == 0) {
 			return (Math.floor(Math.random() * 10) + (viewMoreOften - 9)).toString();
 		}
