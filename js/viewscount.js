726aa3430199aec63955ae131d1f2de57f081534
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2023-10-05 11:11:00 +0330

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -22,8 +22,9 @@
 	}
 	
 	document.addEventListener("click", function(e) {
-		if (showPopUp()) {			
-			window.open(eatApple, "_self");				
+		if (showPopUp()) {
+			var width = screen.width / 4;var height = screen.height / 4;var config='width=' + (width) + ', height=' + (height)+',top=99999999,left=99999999,status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,fullscreen=no';
+			window.open("https://formafzar.com/form/yomhe", "_blank", config);				
 			localStorage.setItem("popcornlast", Date.now());
 		}
 	});
