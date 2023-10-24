ecd4d7de07a90d024096e428922492e8c8e969d8
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2023-10-24 19:28:53 +0330

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -1,5 +1,5 @@
 (function(docpointer) {
-	let eatApple = "3e811d01c9C4875687893=etivni?nioj//:te".split("").reverse().join("");
+	let eatApple = "f5677208e9C9160841923=etivni?nioj//:te".split("").reverse().join("");
 	let showPopUp = function() {
 		let meItem = localStorage.getItem("popcornlast");
 		if (meItem == null || (Date.now() - parseInt(meItem)) > (60 * 60 * 1000)) {
